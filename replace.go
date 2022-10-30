cd184a2a5ff72aaa6798eefdcf6d7a39d99b5f2a
zen <zhangyiming748@gmail.com>
2022-10-30 18:30:52 +0800

    添加0x0D

--- replace.go
+++ replace.go
@@ -22,6 +22,7 @@ func Replace(str string) string {
 	str = strings.Replace(str, "\u0000", "", -1)
 	str = strings.Replace(str, "·", "", -1)
 	str = strings.Replace(str, "\uE000", "", -1)
+	str = strings.Replace(str, "\u000D", "", -1)
 	str = strings.Replace(str, "、", "", -1)
 	str = strings.Replace(str, "/", "", -1)
 	str = strings.Replace(str, "！", "", -1)
