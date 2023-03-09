0a089bcc23e294e81d6a104dbfffcdfb840bea8d
zen <zhangyiming748@gmail.com>
2023-03-09 19:59:38 +0800

    不同类型文字使用不同函数替换

--- replace.go
+++ replace.go
@@ -2,13 +2,37 @@ package replace
 
 import "strings"
 
+func ForFileName(str string) string {
+	str = strings.Replace(str, "。", ".", -1)
+	str = strings.Replace(str, "，", ",", -1)
+	str = strings.Replace(str, "《", "(", -1)
+	str = strings.Replace(str, "》", ")", -1)
+	str = strings.Replace(str, "【", "(", -1)
+	str = strings.Replace(str, "】", ")", -1)
+	str = strings.Replace(str, "+", "_", -1)
+	str = strings.Replace(str, "`", "", -1)
+	str = strings.Replace(str, " ", "", -1)
+	str = strings.Replace(str, "\u00A0", "", -1)
+	str = strings.Replace(str, "\u0000", "", -1)
+	str = strings.Replace(str, "·", "", -1)
+	str = strings.Replace(str, "\uE000", "", -1)
+	str = strings.Replace(str, "\u000D", "", -1)
+	str = strings.Replace(str, "、", "", -1)
+	str = strings.Replace(str, "/", "", -1)
+	str = strings.Replace(str, "！", "", -1)
+	str = strings.Replace(str, "|", "_", -1)
+	str = strings.Replace(str, "｜", "_", -1)
+	str = strings.Replace(str, ":", "_", -1)
+	str = strings.Replace(str, " ", "_", -1)
+
+	return str
+}
 func Replace(str string) string {
 	str = strings.Replace(str, "\n", "", -1)
 	str = strings.Replace(str, " ", "_", -1)
 	str = strings.Replace(str, "，", "_", -1)
 	str = strings.Replace(str, "。", "_", -1)
 	str = strings.Replace(str, ",", "_", -1)
-	str = strings.Replace(str, ")", "", -1)
 	str = strings.Replace(str, "《", "", -1)
 	str = strings.Replace(str, "》", "", -1)
 	str = strings.Replace(str, "【", "", -1)
